ddde731f9e8f02b3fbe584473fa777acefea52a5
Kevin Downs <kevin.downs04@gmail.com>
2019-10-24 01:03:49 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,7 +14,8 @@ def print_first_directors_movie_titles
     if directors[director_index][:name] == "Stephen Spielberg"
       movies_index = 0
       while movies_index < directors[director_index][:movies].length
-      puts 
+      puts directors[director_index][:movies][movies_index][:title]
+      movies_index += 1
     end
     director_index += 1
   end
